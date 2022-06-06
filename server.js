3870440e99d136ebc3c6f8ba5caacce1a7ae36d1
Pierre Melhor <pierremmelhor@gmail.com>
2022-06-06 12:19:38 -0300

    Hide MongoDB account information

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { Schema } = mongoose
 
 let mw = bodyParser.urlencoded({extended: false})
 
-process.env.MONGO_URI=''
+process.env.MONGO_URI=' '
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const urlSchema = new Schema({
